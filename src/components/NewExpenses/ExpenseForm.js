5e5c7451feee52b7e9996851720d664cca5d08a5
lakshya <lakshyaa.nautiyal@gmail.com>
2022-09-20 21:51:41 +0530

    Submit form with all inputs

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -1,24 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ExpenseForm.css'
 
 const ExpenseForm = () => {
+    const [enteredtitle, setEnteredtitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
+
+    function titleHandler(e) {
+        setEnteredtitle(e.target.value)
+
+    }
+
+    function amountHandler(e) {
+        setEnteredAmount(e.target.value)
+
+    }
+
+    function dateHandler(e) {
+        setEnteredDate(e.target.value);
+
+    }
+
+
+    function submitHandler(e) {
+        e.preventDefault();
+
+        const expenseData = {
+            title: enteredtitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        }
+        console.log(expenseData);
+
+    }
+
+
+
+
+
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
-                    <input type='text' onChange={(e) => { console.log(e.target.value); }} />
+                    <input type='text' value={enteredtitle} onChange={titleHandler} />
 
                 </div>
                 <div className='new-expense__control'>
                     <label>Amount</label>
-                    <input type='number' min='0.01' step='0.01' />
+                    <input type='number' min='0.01' step='0.01' value={enteredAmount} onChange={amountHandler} />
 
                 </div>
 
                 <div className='new-expense__control'>
                     <label>Date</label>
-                    <input type='date' min='2019-01-01' step='2022-12-31' />
+                    <input type='date' min='2019-01-01' step='2022-12-31' value={enteredDate} onChange={dateHandler} />
 
                 </div>
 
